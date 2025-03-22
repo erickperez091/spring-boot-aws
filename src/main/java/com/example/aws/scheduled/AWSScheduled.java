331e65aa811856c7e5ab41563c483032f0ebcdae
Erick <erickperez091@hotmail.com>
2025-03-22 11:05:21 -0600

    Adding a job running every minute to read messages from SQS Queue

---
+++ src/main/java/com/example/aws/scheduled/AWSScheduled.java
@@ -0,0 +1,45 @@
+package com.example.aws.scheduled;
+
+import com.example.aws.config.AWSSQSConfig;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.Message;
+
+import java.util.List;
+
+@Component
+public class AWSScheduled {
+
+    Logger logger = LoggerFactory.getLogger(AWSScheduled.class);
+
+    @Value( "${cloud.aws.sqs.messageKey}" )
+    private String messageKey;
+
+    private final SqsClient sqsClient;
+    private final AWSSQSConfig awsSQSConfig;
+
+    public AWSScheduled( SqsClient sqsClient, @Qualifier("AWSSQSConfig") AWSSQSConfig awsSQSConfig ) {
+        this.sqsClient = sqsClient;
+        this.awsSQSConfig = awsSQSConfig;
+    }
+
+    @Scheduled( cron = "0 * * * * *")
+    public void readMessage( ) {
+        logger.info( "[START].[AWSScheduled].[readMessage]:Reading messages from sqs queue" );
+        List< Message > messages = this.awsSQSConfig.getMessages( sqsClient );
+        messages.forEach( message -> {
+            logger.info( "[START].[AWSScheduled].[readMessage]:Processing message: " + message.messageId() );
+            JSONObject object = new JSONObject( message.body() );
+            System.out.println( object.has( messageKey ) ? object.getString( messageKey ) : "Not Present" );
+            awsSQSConfig.deleteMessage( sqsClient, message );
+            logger.info( "[END].[AWSScheduled].[readMessage]:Processing message: " + message.messageId() );
+        } );
+        logger.info( "[END].[AWSScheduled].[readMessage]:Reading messages from sqs queue" );
+    }
+}
--- src/main/java/com/example/aws/service/impl/AWSS3ServiceImplV2.java
+++ src/main/java/com/example/aws/service/impl/AWSS3ServiceImplV2.java
@@ -1,13 +1,9 @@
 package com.example.aws.service.impl;
 
 import com.example.aws.config.AWSSNSConfig;
-import com.example.aws.config.AWSSQSConfig;
 import com.example.aws.config.AbstractAwsConfig;
 import com.example.aws.service.DocumentService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,13 +11,10 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.Message;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @ConditionalOnProperty( name = "cloud.aws.sdk.version", havingValue = "V2" )
@@ -30,43 +23,27 @@ public class AWSS3ServiceImplV2 implements DocumentService {
 
     private final AbstractAwsConfig awsS3Config;
     private final AWSSNSConfig awsSNSConfig;
-    private final AWSSQSConfig awsSQSConfig;
     private final S3Client s3Client;
     private final SnsClient snsClient;
-    private final SqsClient sqsClient;
 
-    @Value( "${cloud.aws.sqs.messageKey}" )
-    private String messageKey;
 
-    public AWSS3ServiceImplV2( @Qualifier( value = "AWSS3ConfigV2" ) AbstractAwsConfig awsS3Config, @Qualifier( value = "AWSSNSConfig" ) AWSSNSConfig awsSNSConfig, @Qualifier( value = "AWSSQSConfig" ) AWSSQSConfig awsSQSConfig, S3Client s3Client, SnsClient snsClient, SqsClient sqsClient ) {
+    public AWSS3ServiceImplV2( @Qualifier( value = "AWSS3ConfigV2" ) AbstractAwsConfig awsS3Config, @Qualifier( value = "AWSSNSConfig" ) AWSSNSConfig awsSNSConfig, S3Client s3Client, SnsClient snsClient ) {
         this.awsS3Config = awsS3Config;
         this.awsSNSConfig = awsSNSConfig;
-        this.awsSQSConfig = awsSQSConfig;
         this.s3Client = s3Client;
         this.snsClient = snsClient;
-        this.sqsClient = sqsClient;
     }
 
     @Override
     public void uploadDocument( String fileName, MultipartFile file ) throws IOException {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
         Map< String, String > metadataMap = new HashMap<>();
-
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket( awsS3Config.getBucketName() )
                 .key( fileName )
                 .metadata( metadataMap )
                 .build();
-
         s3Client.putObject( putObjectRequest, RequestBody.fromInputStream( file.getInputStream(), file.getSize() ) );
         this.awsSNSConfig.sendMessage( fileName, snsClient );
-        List< Message > messages = this.awsSQSConfig.getMessages( sqsClient );
-        messages.forEach( message -> {
-            JSONObject object = new JSONObject( message.body() );
-            System.out.println( object.has( messageKey ) ? object.getString( messageKey ) : "Not Present" );
-        } );
     }
 
     @Override
